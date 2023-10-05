e49d64b2a118504b90c36c09e31d2f0554c0938b
danilocalixto77 <danilocalixto@gmail.com>
2023-10-05 11:19:56 -0400

    202310051119

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,6 +6,11 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
             Console.WriteLine("Primeiro Valor: ");
             float valor1 = float.Parse(Console.ReadLine());
@@ -16,10 +21,10 @@ namespace Calculadora
             Console.WriteLine("");
 
             float resultado = valor1 + valor2;
-            Console.WriteLine("O resultado da soma é: ");
-            Console.WriteLine(resultado);
+            // Console.WriteLine("O resultado da soma é: ");
+            // Console.WriteLine(resultado);
             Console.WriteLine($"O resultado da soma é: {resultado}");
-            Console.WriteLine(valor1 + valor2);
+            // Console.WriteLine(valor1 + valor2);
         }
     }
 }
