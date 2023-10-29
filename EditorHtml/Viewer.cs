27e2d6d0191f42d8694742fcac0bd25e10f70f71
danilocalixto77 <danilocalixto@gmail.com>
2023-10-28 21:21:42 -0400

    feet: final do editor html console

--- EditorHtml/Viewer.cs
+++ EditorHtml/Viewer.cs
@@ -26,7 +26,7 @@ namespace EditorHtml
             //Console.WriteLine(strong);
             var words = text.Split(' ');
 
-            for (var i = 0; i <= words.Length; i++)
+            for (var i = 0; i < words.Length; i++)
             {
                 if (strong.IsMatch(words[i]))
                 {
@@ -35,11 +35,17 @@ namespace EditorHtml
                         words[i].Substring(
                             words[i].IndexOf('>') + 1,
                             (
-                            words[i].LastIndexOf('<') - 1) -
-                            words[i].LastIndexOf('>')
+                                (words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>')
                             )
-                        );
-
+                        )
+                    );
+                    Console.Write("");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write("");
                 }
             }
 
