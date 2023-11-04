3b71df26833305b143d12203d8ff1c908a8c430f
danilocalixto77 <danilocalixto@gmail.com>
2023-11-04 09:09:38 -0400

    feet: 202311040909

--- DotnetArrayLista/Program.cs
+++ DotnetArrayLista/Program.cs
@@ -8,13 +8,25 @@ namespace DotnetArrayLista
         static void Main(string[] args)
         {
             Console.Clear();
+            //ModuloAula1301();
+            //ModuloAula1302();
+            ModuloAula1303();
 
+        }
+
+        struct Teste
+        {
+            public int Id { get; set; }
+        }
+
+        //Modulo: 13 - Arrays | Aula: 01 - Arrays
+        static void ModuloAula1301()
+        {
             //Formas de inicialização de array:
             int[] meuArray = new int[5];
             var meuArray1 = new int[4];
             //Inicializando com valores. 
             var meuArrayInicializado = new int[5] { 1, 10, 100, 200, 300 };
-
 
             Console.WriteLine(meuArray[0]);
 
@@ -26,33 +38,10 @@ namespace DotnetArrayLista
 
             Console.WriteLine(meuArray[0]);
             Console.WriteLine(meuArray[1]);
-
             var meuArrayStructEx = new Teste[2] { new Teste(), new Teste() };
-
-            Console.WriteLine(meuArrayStructEx[0].Id);
-
-            for (int i = 0; i < meuArray.Length; i++)
-            {
-                Console.WriteLine($"Posição: {i} Valor: {meuArray[i]}");
-
-            }
-
-            foreach (var item in meuArray)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
-
         }
 
-        struct Teste
-        {
-            public int Id { get; set; }
-        }
-
-
+        //Modulo: 13 - Arrays | Aula 02: Percorrendo um array
         static void ModuloAula1302()
         {
             int[] meuArray = new int[5];
@@ -75,12 +64,9 @@ namespace DotnetArrayLista
                 Console.WriteLine($"Posição: {i} Valor: {meuArray[i]}");
 
             }
-
-            ModuloAula1303();
-
         }
 
-
+        //Modulo: 13 - Arrays | Aula 03: ForEach
         static void ModuloAula1303()
         {
             int[] meuArray = new int[5];
@@ -90,12 +76,21 @@ namespace DotnetArrayLista
             meuArray[3] = 200;
             meuArray[4] = 1200;
 
+            Console.WriteLine("ForEach de um array de inteiros");
+            Console.WriteLine("----------------------------------");
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("ForEach de um array de inteiros");
+            Console.WriteLine("----------------------------------");
             var funcionarios = new Funcinario[5];
             funcionarios[0] = new Funcinario() { Id = 1010, Nome = "Danilo" };
             funcionarios[1] = new Funcinario() { Id = 2020, Nome = "Lu" };
             foreach (var funcionario in funcionarios)
             {
-                Console.WriteLine($"{funcionario.Id} - {funcionario.Nome}");
+                Console.WriteLine($"ID: {funcionario.Id} - NOME: {funcionario.Nome}");
             }
         }
 
